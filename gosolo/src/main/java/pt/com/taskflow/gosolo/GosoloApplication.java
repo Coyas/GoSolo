08fd3b6e5ef14b4236072f9ff4aa4faa841e8b89
Ailton Duarte <adidas.coyas@gmail.com>
2026-05-13 15:03:45 +0100

    feat: start spring boot BEM

---
+++ gosolo/src/main/java/pt/com/taskflow/gosolo/GosoloApplication.java
@@ -0,0 +1,13 @@
+package pt.com.taskflow.gosolo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GosoloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GosoloApplication.class, args);
+	}
+
+}
---
+++ gosolo/src/test/java/pt/com/taskflow/gosolo/GosoloApplicationTests.java
@@ -0,0 +1,13 @@
+package pt.com.taskflow.gosolo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GosoloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
