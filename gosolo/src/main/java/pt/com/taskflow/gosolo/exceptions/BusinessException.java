4f3599122e15665e976692c6c8ad8fa27dd3e32e
Ailton Duarte <adidas.coyas@gmail.com>
2026-05-15 16:41:07 +0100

    feat: apoio a excecoes globais

---
+++ gosolo/src/main/java/pt/com/taskflow/gosolo/exceptions/BusinessException.java
@@ -0,0 +1,8 @@
+package pt.com.taskflow.gosolo.exceptions;
+
+public class BusinessException extends RuntimeException {
+
+    public BusinessException(String message) {
+        super(message);
+    }
+}
