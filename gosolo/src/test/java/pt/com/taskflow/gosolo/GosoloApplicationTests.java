840295dc360ab52c1a9c0f2b4e252d6985fbf9af
Ailton Duarte <adidas.coyas@gmail.com>
2026-05-15 17:46:48 +0100

    feat: test configs

--- gosolo/src/test/java/pt/com/taskflow/gosolo/GosoloApplicationTests.java
+++ gosolo/src/test/java/pt/com/taskflow/gosolo/GosoloApplicationTests.java
@@ -1,10 +1,26 @@
 package pt.com.taskflow.gosolo;
 
+import org.flywaydb.core.Flyway;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(properties = {
+		"spring.datasource.url=jdbc:h2:mem:testdb;DB_CLOSE_DELAY=-1",
+		"spring.datasource.username=sa",
+		"spring.datasource.password=",
+		"spring.datasource.driver-class-name=org.h2.Driver",
+		"spring.flyway.enabled=false",
+		"spring.jpa.hibernate.ddl-auto=create-drop",
+		"jwt.secret=test-secret-key-for-testing-purposes-only-minimum-256-bits",
+		"jwt.expiration=86400000"
+})
 class GosoloApplicationTests {
+
+	@MockitoBean
+	Flyway flyway;
 
 	@Test
 	void contextLoads() {
